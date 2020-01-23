edab9ca88059d552ab16a6ae8d32edc49e5dc0f8
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2020-01-24 00:06:01 +0400

    Configuring to listen to port 7007 (for HTTP connetions)

--- sample-project/src/main/java/com/arsdever/HelloWorldApplication.java
+++ sample-project/src/main/java/com/arsdever/HelloWorldApplication.java
@@ -2,13 +2,7 @@ package com.arsdever;
 
 import com.arsdever.health.TemplateHealthCheck;
 import com.arsdever.resources.HelloWorldResource;
-import com.google.common.base.Charsets;
-import com.google.common.cache.CacheBuilderSpec;
 import io.dropwizard.Application;
-import io.dropwizard.ConfiguredBundle;
-import io.dropwizard.bundles.assets.AssetServlet;
-import io.dropwizard.bundles.assets.AssetsBundleConfiguration;
-import io.dropwizard.bundles.assets.AssetsConfiguration;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.swagger.config.ScannerFactory;
@@ -16,9 +10,6 @@ import io.swagger.jaxrs.config.BeanConfig;
 import io.swagger.jaxrs.config.DefaultJaxrsScanner;
 import io.swagger.jaxrs.listing.ApiListingResource;
 import io.swagger.jaxrs.listing.SwaggerSerializers;
-
-import java.util.Iterator;
-import java.util.Map;
 
 public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
     public static void main(String[] args) throws Exception {
@@ -32,42 +23,6 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration> 
 
     @Override
     public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
-        ConfiguredBundle bundle = new ConfiguredBundle("/swagger-ui", "/api/", "index.html") {
-
-            public void initialize(Bootstrap<?> bootstrap) {
-            }
-
-            public void run(AssetsBundleConfiguration bundleConfig, Environment env) throws Exception {
-                AssetsConfiguration config = bundleConfig.getAssetsConfiguration();
-                CacheBuilderSpec spec = config.getCacheSpec() != null ? CacheBuilderSpec.parse(config.getCacheSpec()) : this.cacheBuilderSpec;
-                Iterable<Map.Entry<String, String>> overrides = config.getOverrides().entrySet();
-                Iterable<Map.Entry<String, String>> mimeTypes = config.getMimeTypes().entrySet();
-                Object servletResourcePathToUriMappings;
-                if (!config.getResourcePathToUriMappings().isEmpty()) {
-                    servletResourcePathToUriMappings = config.getResourcePathToUriMappings().entrySet();
-                } else {
-                    servletResourcePathToUriMappings = this.resourcePathToUriMappings;
-                }
-
-                AssetServlet servlet = new AssetServlet((Iterable)servletResourcePathToUriMappings, this.indexFile, Charsets.UTF_8, spec, overrides, mimeTypes);
-                Iterator var9 = ((Iterable)servletResourcePathToUriMappings).iterator();
-
-                while(var9.hasNext()) {
-                    Map.Entry<String, String> mapping = (Map.Entry)var9.next();
-                    String mappingPath = (String)mapping.getValue();
-                    if (!mappingPath.endsWith("/")) {
-                        mappingPath = mappingPath + '/';
-                    }
-
-                    mappingPath = mappingPath + "*";
-                    servlet.setCacheControlHeader(config.getCacheControlHeader());
-                    LOGGER.info("Registering ConfiguredAssetBundle with name: {} for path {}", this.assetsName, mappingPath);
-                    env.servlets().addServlet(this.assetsName, servlet).addMapping(new String[]{mappingPath});
-                }
-
-            }
-        };
-        bootstrap.addBundle();
     }
 
     private void initSwagger(HelloWorldConfiguration configuration, Environment environment) {
@@ -85,7 +40,7 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration> 
         //This is what is shown when you do "http://localhost:8080/swagger-ui/"
         BeanConfig beanConfig = new BeanConfig();
         beanConfig.setVersion(version);
-        beanConfig.setDescription("The drpowizard apis");
+        beanConfig.setDescription("The dropwizard apis");
         beanConfig.setResourcePackage("ca.gaudreault.mydropwizardapp");
         beanConfig.setScan(true);
     }
